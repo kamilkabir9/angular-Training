4e93fe80b629dadd0a961423f092d76ef2aff61f
vinodhleo <vinodhleo@outlook.com>
2022-04-07 14:46:12 +0530

    service-commit

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+ httpOptions:any;
+ username:String='';
+ usertype:String='';
+ token:any='';
+ email:any='';
+ status:boolean=false;
+  constructor(private http:HttpClient) {
+      let username=sessionStorage.getItem("username");
+      let usertype=sessionStorage.getItem("usertype");
+      let token=sessionStorage.getItem("token");
+      let email=sessionStorage.getItem("email");
+      if(username&&usertype){
+        this.status=true;
+        this.username=username;
+        this.usertype=usertype;
+        this.token=token;
+        this.email=email;
+      }
+   }
+
+  getStatus():boolean{
+    return this.status;
+  }
+
+  // To Register a User
+  register(obj:any):Observable<object>{
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+        })
+    }
+    return this.http.post("http://localhost:4500/register",obj,this.httpOptions);
+  }
+
+  //To check avalailability of mail id and username
+
+  check(str:any):Observable<object>{
+    return this.http.get("http://localhost:4500/users?firstname="+str,this.httpOptions);
+  }
+
+// to Login
+  login(email:any,password:any):Observable<object>{
+ 
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+  
+      })
+    }
+    return this.http.post("http://localhost:4500/login",
+    {email:email,password:password},
+    this.httpOptions);
+  }
+
+  logout():void{
+     this.token="";
+     this.username="";
+     this.usertype="";
+     sessionStorage.removeItem("token");
+     sessionStorage.removeItem("username");
+     sessionStorage.removeItem("usertype");
+     sessionStorage.removeItem("email");
+  }
+}
