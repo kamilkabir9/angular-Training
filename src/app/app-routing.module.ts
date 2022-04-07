955ea9a9fb0c53b90b054bc6fe5f2dc8f5cc58f2
vinodhleo <vinodhleo@outlook.com>
2022-04-07 14:33:22 +0530

    code-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"user",
+  loadChildren:() => import('./user/user.module').then(m => m.UserModule)},
+  {path:"admin",loadChildren: 
+  () => import('./admin/admin.module').then(m => m.AdminModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
